72b5eaa6fef6ceffcf00e92dbb4078e781835294
liren.lrl <liren.lrl@alibaba-inc.com>
2015-05-07 16:36:30 +0800

    commit my first test code with Robolectric

--- android-api-19/src/main/java/com/example/activity/MainActivity.java
+++ android-api-19/src/main/java/com/example/activity/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.example.R;
 
 public class MainActivity extends Activity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
-    setTitle("Deckard");
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        setTitle("Deckard");
+
+        final View button = findViewById(R.id.button1);
+        button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
 }
--- android-api-19/src/test/java/com/example/activity/MainActivityTest.java
+++ android-api-19/src/test/java/com/example/activity/MainActivityTest.java
@@ -1,8 +1,10 @@
 package com.example.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 
 import com.example.BuildConfig;
+import com.example.R;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,7 +12,9 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.robolectric.Shadows.shadowOf;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class)
@@ -20,5 +24,15 @@ public class MainActivityTest {
   public void titleIsCorrect() throws Exception {
     Activity activity = Robolectric.setupActivity(MainActivity.class);
     assertTrue(activity.getTitle().toString().equals("Deckard"));
+  }
+
+  @Test
+  public void clickLogin_shouldStartLoginActivity() {
+    Activity activity = Robolectric.setupActivity(MainActivity.class);
+    activity.findViewById(R.id.button1).performClick();
+
+    Intent i = new Intent(activity, LoginActivity.class);
+
+    assertTrue(shadowOf(activity).getNextStartedActivity().equals(i));
   }
 }
